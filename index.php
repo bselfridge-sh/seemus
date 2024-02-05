e1c47797c0439406b506a7ef456d7b7f4c0d791a
Bob Selfridge <bselfridge@southhills.edu>
2024-02-05 11:34:28 -0500

    Updated!

--- index.php
+++ index.php
@@ -1,42 +1,7 @@
 <?php
 include "include/db.inc.php";
+include "class/Utils.class.php";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Successful connection";
-} catch(PDOException $e) {
-    echo $e->getMessage();
-}
-
-$stmt = $conn->prepare("SELECT * FROM sys.host_summary;");
-$stmt->execute();
-$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-if ($result) {
-  $printedHeader=false;
-  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    if(!$printedHeader) {
-      echo "<table border=1>";   // Start Table
-      echo "<tr>";      // Start Header Row
-      foreach($row as $col_name => $val)
-        {
-        echo "<th>$col_name</th>";    // Print Each Field Name
-        }
-      echo "</tr>";
-      $printedHeader=true;
-    }
-    echo "<tr>";      // Start Header Row
-    foreach($row as $col_name => $val)
-      {
-      echo "<td>$val</td>";    // Print Each Field Name
-      }
-      echo "</tr>";      // Start Header Row
-}
-  echo "</table>";
-} else {
-  echo "0 results";
-}
 
 
 include "include/dbclose.inc.php";
