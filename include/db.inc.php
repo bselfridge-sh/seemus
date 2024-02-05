ba8ac122149a9c2895c41a1461ea227cbfd18c71
Bob Selfridge <bselfridge@southhills.edu>
2024-02-05 10:19:47 -0500

    New Files

--- include/db.inc.php
+++ include/db.inc.php
@@ -1,8 +1,8 @@
 <?php
 
   $servername = "localhost";
-  $username = "selfridge";
-  $password = "xpsr450D$";
+  $username = "shuser";
+  $password = "shRocks!";
   $dbname = "Seemus";
   
 try {
@@ -11,35 +11,6 @@ try {
     echo "Successful connection";
 } catch(PDOException $e) {
     echo $e->getMessage();
-}
-
-$stmt = $conn->prepare("SELECT `ID`, `IDauto`,`Name` FROM Seemus.tbTable");
-$stmt->execute();
-$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-if ($result) {
-  $printedHeader=false;
-  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    if(!$printedHeader) {
-      echo "<table>";   // Start Table
-      echo "<tr>";      // Start Header Row
-        foreach($row as $col_name => $val)
-          {
-          echo "<th>$col_name</th>";    // Print Each Field Name
-          }
-      echo "</tr>";
-      $printedHeader=true;
-    }
-    echo "<tr>";      // Start Header Row
-    foreach($row as $col_name => $val)
-      {
-      echo "<td>$val</td>";    // Print Each Field Name
-      }
-      echo "</tr>";      // Start Header Row
-}
-  echo "</table>";
-} else {
-  echo "0 results";
 }
 
 ?>
