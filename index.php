087e428be9da0a7412ff077be3cb4864b10342e8
bselfridge-sh <151676079+bselfridge-sh@users.noreply.github.com>
2024-02-13 10:19:30 -0500

    a

--- index.php
+++ index.php
@@ -8,9 +8,10 @@ $activity = formRequest("activity");
 <html>
     <head><title>Seemus</title></head>
     <body>
-        <a href="index.php?activity=USER">LOGON</a> | 
+        <div class="navigation"><a href="index.php?activity=USER">LOGON</a> | 
         <a href="index.php?activity=USER-LOGOFF">LOGOFF</a> | 
-        <br><br>
+        </div>
+        <div class="reason">
         <?php
             if(formRequest("reason")=="") {
                 echo "<br>";
@@ -18,18 +19,16 @@ $activity = formRequest("activity");
                 echo "<em>" . formRequest("reason") . "</em><br>";
             }
         ?>
+        </div>
         <br>
         <?php
         switch($activity) {
             case "USER": // User Logon
                 if(formRequest("email") == "") {
-                    if(formRequest("reason")!="") {
-                        
-                    }
                     ?>
                     <form action="index.php" method=post >
                         <input type="hidden" name="activity" value="USER" />
-                        <input type="text" name="email" placeholder="Email" />
+                        <input type="text" name="email" placeholder="Email" value="<?php echo formRequest("email_last"); ?>" />
                         <input type="text" name="password" placeholder="Password" />
                         <input type="submit" value="Logon" />
                     </form>
@@ -38,7 +37,7 @@ $activity = formRequest("activity");
                 } else {
                     $stmt = $conn->prepare("SELECT * FROM tbUsers WHERE fdEmail='".formRequest("email")."'");
                     $stmt->execute();
-                    $result = $stmt->fetchAll();
+                    $result = $stmt->fetchAll(); // Returns true/false if records exist
 
                     if($result) {
                         foreach($result as $row) {
@@ -50,11 +49,11 @@ $activity = formRequest("activity");
                                 $_SESSION["Email"]      = $row["fdEmail"];
                                 $_SESSION["Admin"]      = $row["fdAdmin"];
                             } else {
-                                echo('<script>window.location=\'index.php?activity=USER&reason=Bad+Username+or+Password\';</script>');
+                                echo('<script>window.location=\'index.php?activity=USER&email_last='.formRequest("email").'&reason=Bad+Email+or+Bad+Password\';</script>');
                             }
                         }
                     } else {
-                        echo('<script>window.location=\'index.php?activity=USER&reason=Bad+Username+or+Password\';</script>');
+                        echo('<script>window.location=\'index.php?activity=USER&email_last='.formRequest("email").'&reason=Wrong+Email+or+Bad+Password\';</script>');
                     }
                 }
             break;
