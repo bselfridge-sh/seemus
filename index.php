4d3d361ad6d31f9124c47bd1b581d780a7d84f0f
Bob Selfridge <bselfridge@southhills.edu>
2024-02-21 11:22:50 -0500

    a

--- index.php
+++ index.php
@@ -54,9 +54,57 @@ $activity = formRequest("activity");
             }
         switch($activity) {
             case "FILE-CREATE-PROCESS":
-                echo $activity."<BR>";
+                // echo $_FILES['File']["name"];
 
-                echo $_FILES['File']["name"];
+                $target_dir = "/file_uploads/";
+                $target_file = $target_dir . basename($_FILES["File"]["name"]);
+                $uploadOk = 1;
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                
+                // Check if image file is a actual image or fake image
+                if(isset($_POST["Submit"])) {
+                  $check = getimagesize($_FILES["File"]["tmp_name"]);
+                  if($check !== false) {
+                    echo "File is an image - " . $check["mime"] . ".";
+                    $uploadOk = 1;
+                  } else {
+                    echo "File is not an image.";
+                    $uploadOk = 0;
+                  }
+                }
+                
+                // Check if file already exists
+                if (file_exists($target_file)) {
+                  echo "Sorry, file already exists.";
+                  $uploadOk = 0;
+                }
+                
+                // Check file size
+                if ($_FILES["File"]["size"] > 5000000) {
+                  echo "Sorry, your file is too large.";
+                  $uploadOk = 0;
+                }
+                
+                // Allow certain file formats
+                if($imageFileType != "pdf" && $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                  echo "Sorry, only PDF, JPG, JPEG, PNG & GIF files are allowed.";
+                  $uploadOk = 0;
+                }
+                
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                  echo "Sorry, your file was not uploaded.";
+                // if everything is ok, try to upload file
+                } else {
+                  if (move_uploaded_file($_FILES["File"]["tmp_name"], $target_file)) {
+                    echo "The file ". htmlspecialchars( basename( $_FILES["File"]["name"])). " has been uploaded.";
+                  } else {
+                    echo "Sorry, there was an error uploading your file.";
+                  }
+                }
+
+
 
             case "FILES": // File Listing
 
@@ -65,7 +113,7 @@ $activity = formRequest("activity");
                 <input type="hidden" name="activity" value="FILE-CREATE-PROCESS">
                 <input type="hidden" name="order" value="<?php echo formRequest("order"); ?>">
                 <input type="file" name="File" placeholder="File" value="">
-                <input type="submit" value="UPLOAD!"><br>
+                <input type="submit" name="Submit" value="UPLOAD!"><br>
                 </form>
                 <?php
 
