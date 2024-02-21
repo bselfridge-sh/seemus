f3707c5243b5af630ad3daf2ac2bfc32d126a9f3
Bob Selfridge <bselfridge@southhills.edu>
2024-02-21 13:01:32 -0500

    Do It

---
+++ view.php
@@ -0,0 +1,22 @@
+<?php
+include "include/top.inc.php";
+include "class/Utils.class.php";
+
+$activity = formRequest("activity");
+
+$sql = "SELECT id,fdFile, fdFilename,fdFileType,fdFileSize,fdDateTime,fdArchive FROM `tbFiles` WHERE id = " . formRequest("id");
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+// Check if $result has anything in it or not (Returns a FALSE if no data in there).
+if($result) {
+    header("Content-Type: image/jpeg");
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo $row["fdFile:"];
+    }
+}
+
+    include "include/bottom.inc.php";
+?>
